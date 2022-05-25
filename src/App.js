a2093d64e3a8f8aadd1dd84d759c66b581475d54
Ian Whitmore <ianwhitmor@gmail.com>
2022-05-25 12:25:35 -0500

    Create Header component and fill with starter text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Hello MOO
-        </p>
-      </header>
-    </div>
+    <>
+    <Header />
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return(
+    <div>
+      <h1>
+        Sarah Whitmore!
+      </h1>
+    </div>
+  )
+}
+
+export default Header;
